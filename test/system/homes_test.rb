d337c024d85a59076f0002ed26d6bfe2be740715
Leonard <leonard.horne@gmail.com>
2017-09-28 17:30:05 -0400

    Add add home system test.

--- test/system/homes_test.rb
+++ test/system/homes_test.rb
@@ -6,4 +6,17 @@ class HomesTest < ApplicationSystemTestCase
 
     assert_selector "h1", text: "Home"
   end
+
+  test "add home" do
+    visit homes_url
+
+    click_on "New Home"
+
+    fill_in "Title", with: "Yuss"
+    fill_in "Body", with: "Woohoo!"
+
+    click_on "Create Home"
+
+    assert_text "Yuss"
+  end
 end
